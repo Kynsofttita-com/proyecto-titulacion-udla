3195846818439962f1219eab8df34202c98707dd
Sebastian Cruz <sebascruzb03@hotmail.com>
2026-07-16 13:24:11 -0500

    Sprint 11 (Fix: Actualizar puerto de ms-vehiculos en VehiculosClient a 8084)

--- backend/ms-reportes/src/main/java/com/escuela/reportes/client/VehiculosClient.java
+++ backend/ms-reportes/src/main/java/com/escuela/reportes/client/VehiculosClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "ms-vehiculos", url = "${app.services.vehiculos.url:http://ms-vehiculos:8085}")
+@FeignClient(name = "ms-vehiculos", url = "${app.services.vehiculos.url:http://ms-vehiculos:8084}")
 public interface VehiculosClient {
 
     @GetMapping("/vehiculos")
